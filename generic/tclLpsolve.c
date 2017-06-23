5f62509a15abb15e67dd8a923b8aab31cdb8b08d
Eckhard Lehmann <Eckhard.Lehmann@dermalog.com>
2017-06-23 14:35:13 +0200

    MSVC project updated

--- generic/tclLpsolve.c
+++ generic/tclLpsolve.c
@@ -9,21 +9,9 @@
  */
 
 #include <tcl.h>
-#include "sample.h"
 
 static int
-Lrec_Cmd(
-    ClientData clientData,	/* Not used. */
-    Tcl_Interp *interp,		/* Current interpreter */
-    int objc,			/* Number of arguments */
-    Tcl_Obj *const objv[]	/* Argument strings */
-    )
-{
-   return TCL_ERROR;
-}
-
-static int
-Lpsolve_Cmd(
+Lp_Cmd(
     ClientData clientData,	/* Not used. */
     Tcl_Interp *interp,		/* Current interpreter */
     int objc,			/* Number of arguments */
@@ -52,24 +40,35 @@ Lpsolve_Cmd(
  *
  *----------------------------------------------------------------------
  */
+static void
+LpsolveDeleteNamespace(ClientData data)
+{
 
-int
+}
+
+DLLEXPORT int
 Lpsolve_Init(Tcl_Interp *interp)
 {
+    Tcl_Namespace* lpNS = NULL;
+
 #ifdef USE_TCL_STUBS
     if (Tcl_InitStubs(interp, "8.1", 0) == NULL) {
         return TCL_ERROR;
     }
 #endif //USE_TCL_STUBS
 
-    if (Tcl_PkgProvide(interp, PACKAGE_NAME, PACKAGE_VERSION) != TCL_OK) {
+    if ((lpNS = Tcl_CreateNamespace(interp, "lpsolve", (ClientData)NULL, LpsolveDeleteNamespace)) == NULL) {
+        Tcl_SetObjResult(interp, Tcl_NewStringObj("Cannot create ::lpsolve namespace", -1));
         return TCL_ERROR;
     }
 
-    Tcl_CreateObjCommand(interp, "lprec", (Tcl_ObjCmdProc *) Lprec_Cmd,
-	    (ClientData)NULL, (Tcl_CmdDeleteProc *)NULL);
-    Tcl_CreateObjCommand(interp, "lpsolve", (Tcl_ObjCmdProc *) Lpsolve_Cmd,
-	    (ClientData)NULL, (Tcl_CmdDeleteProc *)NULL);
+    Tcl_CreateObjCommand(interp, "::lpsolve::lp", (Tcl_ObjCmdProc *) Lp_Cmd,
+        (ClientData)NULL, (Tcl_CmdDeleteProc *)NULL);
+    Tcl_Export(interp, lpNS, "::lpsolve::lp", 0);
+
+    if (Tcl_PkgProvide(interp, PACKAGE_NAME, PACKAGE_VERSION) != TCL_OK) {
+        return TCL_ERROR;
+    }
 
     return TCL_OK;
 }
